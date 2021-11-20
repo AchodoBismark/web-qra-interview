aab26bb3e1546dd00d386d48a73678ad82d96e45
Bismark Achodo <achodobismark@gmail.com>
2021-11-20 09:09:03 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,23 @@
+let card = document.getElementsByClassName('score__item');
+let score = document.getElementsByClassName('score__item__btn');
+let closeWindow = document.getElementById('closewindow');
+
+closeWindow.addEventListener('click', ()=>{ 
+    console.log("clicked")
+    window.close();
+
+});
+
+
+
+for (let i = 0; i < score.length; i++) {
+    score[i].addEventListener('click', ()=>{
+        score[i].parentElement.classList.toggle('active__score__item')
+        let details = score[i].nextElementSibling;
+        if(details.style.maxHeight) {
+            details.style.maxHeight = null;
+        }else {
+            details.style.maxHeight = details.scrollHeight + 'px';
+        }
+    });
+}
